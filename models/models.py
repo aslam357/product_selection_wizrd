f38f2cef8203358ca4b18370bc348e1469e76715
aslam <contact.muhammedaslam@fmail.com>
2024-08-21 17:20:10 +0530

    first commit

--- models/models.py
+++ models/models.py
@@ -19,22 +19,18 @@ class ProductSelectionWizard(models.TransientModel):
     _name = 'product.selection.wizard'
     _description = 'Wizard to Select Products'
 
-    sale_order_id = fields.Many2one(
-            'sale.order', 
-            string='Sale Order')
-    product_ids = fields.Many2many(
-            'product.product', 
-            string='Products')
+    sale_order_id = fields.Many2one('sale.order', string='Sale Order')
+    product_ids = fields.Many2many('product.product', string='Products')
 
     def action_add_products_to_sale_order(self):
-        sale_order = self.sale_order_id
-        order_lines = [(0, 0, {
-            'order_id': sale_order.id,
-            'product_id': product.id,
-            'product_uom_qty': 1,
-            'price_unit': product.list_price,
-        }) for product in self.product_ids]
-        sale_order.write({'order_line': order_lines})
+        if self.sale_order_id:
+            sale_order = self.sale_order_id
+            order_lines = [(0, 0, {
+                'product_id': product.id,
+                'product_uom_qty': 1,
+                'price_unit': product.list_price,
+            }) for product in self.product_ids]
+            sale_order.write({'order_line': order_lines})
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
@@ -46,7 +42,10 @@ class ProductTemplate(models.Model):
             'view_mode': 'tree',
             'res_model': 'product.template',
             'domain': [],
-            'context': {'wizard_sale_order_id': self.env.context.get('default_sale_order_id')},
+            'context': {
+                'default_sale_order_id': self.env.context.get('default_sale_order_id')
+            },
             'target': 'new',
-            'multi': 'true'
+            'multi': True
         }
+
